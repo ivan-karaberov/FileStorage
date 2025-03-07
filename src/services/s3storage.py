a93b2c830a875c54ecdba5d03d64de5e59539953
Ivan <karaberov.ivan@yandex.ru>
2025-03-07 13:44:53 +0000

    feat: add getting a link to a file in minio

--- src/services/s3storage.py
+++ src/services/s3storage.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import timedelta
 from abc import ABC, abstractmethod
 
 from minio.error import S3Error
@@ -9,6 +10,10 @@ logger = logging.getLogger(__name__)
 class AbstractS3Storage(ABC):
     @abstractmethod
     def upload_file(self, file: str, bucket_name: str, object_name: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_file_link(self, bucket_name: str, object_name: str) -> str | None:
         raise NotImplementedError
 
     @abstractmethod
@@ -36,15 +41,27 @@ class MinioStorage(AbstractS3Storage):
         except Exception as e:
             logger.error("An unexpected error occurred: %s", e)
             return False
+        
+    def get_file_link(self, bucket_name: str, object_name: str) -> str | None:
+        try:
+            link = self.minio_client.presigned_get_object(bucket_name, object_name, expires=timedelta(hours=12))
+            return link
+
+        except S3Error as e:
+            logger.error("Unsuccessful getting file from MinIO: %s", e)
+            return None
+
+        except Exception as e:
+            logger.error("An unexpected error occured: %s", e)
+            return None
 
     def delete_file(self, bucket_name: str, object_name: str) -> bool:
         try:
-            self.minio_client.remove_object(bucket_name, object_name)
-
+            status = self.minio_client.remove_object(bucket_name, object_name)
             return True
 
         except S3Error as e:
-            logger.error("Unsuccessful delete in MinIO: %s", e)
+            logger.error("Unsuccessful file delete in MinIO: %s", e)
             return False
 
         except Exception as e:
